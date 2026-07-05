ad242b14cda88d031093154c10605cb3bf222929
Mahendra G L <133708107+MahendraGL@users.noreply.github.com>
2026-07-06 00:09:06 +0530

    Implement Digit classifier (Training 4/4)

---
+++ foundations/digit_classifier.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+from torchtyping import TensorType
+
+class Solution(nn.Module):
+    def __init__(self):
+        super().__init__()
+        torch.manual_seed(0)
+        # Architecture: Linear(784, 512) -> ReLU -> Dropout(0.2) -> Linear(512, 10) -> Sigmoid
+        self.model = nn.Sequential(
+            nn.Linear(784, 512),
+            nn.ReLU(),
+            nn.Dropout(p = 0.2),
+            nn.Linear(512, 10)
+        )
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, images: TensorType[float]) -> TensorType[float]:
+        torch.manual_seed(0)
+        # images shape: (batch_size, 784)
+        # Return the model's prediction to 4 decimal places
+        x = self.model(images)
+        x = self.sigmoid(x)
+
+        return torch.round(x, decimals = 4)
+
+
